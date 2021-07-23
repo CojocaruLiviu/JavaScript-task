9ac165e7378c13f457227f5b835a34f1ffb01cb8
CojocaruLiviu <71759748+CojocaruLiviu@users.noreply.github.com>
2021-07-23 11:43:16 +0300

    I do ex 7.5

---
+++ 7.5/main.js
@@ -0,0 +1,14 @@
+document.getElementById("asc").onclick = function() {Incriment()};
+document.getElementById("desc").onclick = function() {Decrement()};
+
+function Incriment() {
+    document.getElementById("num").stepUp();
+  }
+
+function Decrement() {
+    document.getElementById("num").stepDown();
+  }
+  
+
+
+
