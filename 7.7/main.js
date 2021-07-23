a6c71753ce862d1468bdb940a7af9b536daf6810
CojocaruLiviu <71759748+CojocaruLiviu@users.noreply.github.com>
2021-07-23 11:43:55 +0300

    I do ex 7.7

---
+++ 7.7/main.js
@@ -0,0 +1,23 @@
+let addres = [
+  'http://google.com',
+  'directory/page.html',
+  'http://ok.ru',
+  'http://facebook.com',
+  'direct/page.html',
+  'http://youtube.com',
+  'asistance/page1.html'
+  
+]
+
+for (let i = 0; i < addres.length; i++) {
+  
+  for (let j = 0; j < addres[i].length; j++) {
+    if (addres[i][0]=='h' && addres[i][1]=='t' && addres[i][2]=='t' && addres[i][3]=='p' ) {
+  
+      document.getElementById("demo").innerHTML += addres[i++] + '<br>';
+      document.getElementById("demo").style.color = 'red';
+    } else {
+      document.getElementById("demo1").innerHTML += addres[i++] + '<br>';
+      }
+  }  
+}
