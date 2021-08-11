e172123065544dc5564637f3d4140ccb7647bce4
CojocaruLiviu <71759748+CojocaruLiviu@users.noreply.github.com>
2021-08-11 19:50:39 +0300

    I try do ex 7.15 - 7.16

--- 7.15/main.js
+++ 7.15/main.js
@@ -1,8 +1,16 @@
+const numb = document.getElementById('input').value;
+numb = parseInt(numb);
+input.onblur = function () {
+    
+    if (numb < 0) {
+        input.classList.add('invalid');
+        error.innerHTML = 'Input some clear';
+    }
+};
 
-function check(){
-    let val = document.getElementById('valueNumb').value;
-    if (typeof (val) === 'number') {
-        return val;
-    } else document.getElementById('valueNumb').value = '';
-
-}
+input.onfocus = function () {
+    if (this.classList.contains('invalid')) {
+        this.classList.remove('invalid');
+        error.innerHTML = "";
+    }
+};
